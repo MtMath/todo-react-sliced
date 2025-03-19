c23e1ddf90c7b0d795df33b4d86b440fd2b35777
Matheus Costa <matheus.design12@gmail.com>
2025-03-19 01:21:20 -0300

    fix: move auth types to entities

---
+++ src/entities/auth/model/auth.ts
@@ -0,0 +1,13 @@
+export interface LoginCredentials {
+  email: string;
+  password: string;
+  rememberMe?: boolean;
+}
+
+export interface RegisterCredentials {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
