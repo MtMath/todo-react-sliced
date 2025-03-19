8d2ac55580a5754d03d296968f627126e060305f
Matheus Costa <matheus.design12@gmail.com>
2025-03-19 11:58:21 -0300

    chore: use bootstrap icons css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from "./app";
 import { AuthProvider } from "features/auth/index.ts";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./app/styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
